f8a6e13dcb11ce4b68f82f5bb7d8dbebfdc65bea
Kyle Zhao <kylez4@illinois.edu>
2025-03-14 20:10:55 -0700

    New app.js front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Map from "./components/Map";
-
 import './App.css';
 
 function App() {
@@ -9,6 +8,28 @@ function App() {
   const [end, setEnd] = useState("");
   const [stops, setStops] = useState([]);
   const [route, setRoute] = useState(null);
+  const [placeType, setPlaceType] = useState("");
+  const [foundPlaces, setFoundPlaces] = useState([]);
+  const [userLocation, setUserLocation] = useState({
+    lat: 40.1164,
+    lon: -88.2434,
+    name: "Champaign, IL (Default)"
+  });
+
+  // Get User's Current Location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      }
+    );
+  }, []);
 
   const handleAddStop = () => {
     setStops([...stops, ""]);
@@ -33,33 +54,82 @@ function App() {
     }
   };
 
+  const handleFindPlaces = async () => {
+    try {
+      let requestData = { place_type: placeType };
+
+      if (route) {
+        requestData["route"] = route;
+      } else if (userLocation) {
+        requestData["location"] = userLocation;
+      } else {
+        alert("Unable to determine location.");
+        return;
+      }
+
+      const response = await axios.post("http://127.0.0.1:5000/find_places", requestData);
+      setFoundPlaces(response.data);
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
+
+  const addPlaceToStops = (place) => {
+    setStops([...stops, place.name]);
+  };
+
   return (
     <div>
-      <h1 className = "title">Trip Planner</h1>
-      
-      <div className = 'input_start_end'>
-      <input className = 'textBox' type="text" placeholder="Start Location" value={start} onChange={(e) => setStart(e.target.value)} />
-      <input className = 'textBox' type="text" placeholder="End Location" value={end} onChange={(e) => setEnd(e.target.value)} />
+      <h1 className="title">Trip Planner</h1>
+
+      <div className='input_start_end'>
+        <input className='textBox' type="text" placeholder="Start Location" value={start} onChange={(e) => setStart(e.target.value)} />
+        <input className='textBox' type="text" placeholder="End Location" value={end} onChange={(e) => setEnd(e.target.value)} />
       </div>
-  
-      <h3 className = 'stopsTitle'>Stops:</h3>
 
-      <div className = 'input_start_end'>
+      <h3 className='stopsTitle'>Stops:</h3>
+      <div className='input_start_end'>
+        {stops.map((stop, index) => (
+          <input
+            className='stopTextBox'
+            key={index}
+            type="text"
+            placeholder="Stop"
+            value={stop}
+            onChange={(e) => handleStopChange(index, e.target.value)}
+          />
+        ))}
+        <button onClick={handleAddStop}>+ Add Stop</button>
+        <button onClick={handleSubmit}>Get Route</button>
+      </div>
 
-      {stops.map((stop, index) => (
+      {/* Find Places Feature */}
+      <h3 className='stopsTitle'>Find Places:</h3>
+      <div className='input_start_end'>
         <input
-          className = 'stopTextBox'
-          key={index}
+          className='textBox'
           type="text"
-          placeholder="Stop"
-          value={stop}
-          onChange={(e) => handleStopChange(index, e.target.value)}
+          placeholder="Search for places (e.g., gas stations, coffee shops)"
+          value={placeType}
+          onChange={(e) => setPlaceType(e.target.value)}
         />
-      ))}
-      <button onClick={handleAddStop}>+ Add Stop</button>
-      <button onClick={handleSubmit}>Get Route</button>
-
+        <button onClick={handleFindPlaces}>Find Places</button>
       </div>
+
+      {/* Display Found Places */}
+      {foundPlaces.length > 0 && (
+        <div>
+          <h3>Suggested Places:</h3>
+          <ul className="found-places-list">
+            {foundPlaces.map((place, index) => (
+              <li key={index}>
+                {place.name} ({place.lat}, {place.lon})
+                <button onClick={() => addPlaceToStops(place)}>Add to Stops</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <Map route={route} />
     </div>
