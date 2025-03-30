c49cdaea6e1be1d0f028fa6e0ca61f6a126e22b8
Kyle Zhao <kylez4@illinois.edu>
2025-03-30 12:37:19 -0500

    change the test to simulate a location

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Mock the geolocation API
+const mockGeolocation = {
+  getCurrentPosition: jest.fn()
+    .mockImplementation((successCallback) => 
+      successCallback({
+        coords: {
+          latitude: 40.1164,
+          longitude: -88.2434
+        }
+      })
+    )
+};
+
+global.navigator.geolocation = mockGeolocation;
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
